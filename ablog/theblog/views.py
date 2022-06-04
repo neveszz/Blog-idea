149145378fa5a64c9dcfb0783d859731d8d61e8b
neveszz <neveszalves@gmail.com>
2022-06-03 23:15:37 -0300

    Added detail views to the posts and the back anchor link

--- ablog/theblog/views.py
+++ ablog/theblog/views.py
@@ -9,3 +9,7 @@ from django.views.generic import ListView, DetailView
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'article_details.html'
