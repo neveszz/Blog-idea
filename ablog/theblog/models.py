92c025906f65b57829c6cd9ec8ead77db8f9ee42
neveszz <neveszalves@gmail.com>
2022-06-06 06:08:37 -0300

    added the page to create a post

--- ablog/theblog/models.py
+++ ablog/theblog/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+from django.urls import reverse
+
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -10,3 +13,5 @@ class Post(models.Model):
     def __str__(self):
         return self.title + ' | ' + str(self.author)
 
+    def get_absolute_url(self):
+        return reverse('article-detail', args=(str(self.pk)))
--- ablog/theblog/views.py
+++ ablog/theblog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 
 # def home(request):
 #     return render(request, 'home.html',{}) -> old method
@@ -14,3 +14,9 @@ class HomeView(ListView):
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
+
+
+class AddPostView(CreateView):
+    model = Post
+    template_name = 'add_post.html'
+    fields = '__all__'
