927419aa7ed067ab648d50276467bf54ce2c2a53
neveszz <neveszalves@gmail.com>
2022-06-05 21:42:31 -0300

    added title tag and removed some things from navbar

--- ablog/theblog/models.py
+++ ablog/theblog/models.py
@@ -5,6 +5,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
+    title_tag = models.CharField(max_length=255, default=title)
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
