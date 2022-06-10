510e4fee020dbf041bfdbf029bdcf4d6795ae072
neveszz <neveszalves@gmail.com>
2022-06-10 12:01:27 -0300

    category dropdown

--- ablog/theblog/urls.py
+++ ablog/theblog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 # from . import views
-from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView,\
+    CategoryView, CategoryListView
 
 urlpatterns = [
     # path('', views.home, name='home')
@@ -10,7 +11,8 @@ urlpatterns = [
     path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
     path('article/delete/<int:pk>', DeletePostView.as_view(), name='delete_post'),
     path('add_category/', AddCategoryView.as_view(), name='add_category'),
-    path('category/<str:catego>/', CategoryView, name='category')
+    path('category/<str:catego>/', CategoryView, name='category'),
+    path('category_list/', CategoryListView, name='category_list')
 
 
 ]
--- ablog/theblog/views.py
+++ ablog/theblog/views.py
@@ -14,10 +14,21 @@ class HomeView(ListView):
     ordering = ['-id']
     # ordering = ['-post_date']
 
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
+
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
 
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(ArticleDetailView, self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
 
 class AddPostView(CreateView):
     model = Post
@@ -25,16 +36,35 @@ class AddPostView(CreateView):
     template_name = 'add_post.html'
     # fields = '__all__'
 
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(AddPostView, self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
+
 class AddCategoryView(CreateView):
     model = Category
     template_name = 'add_category.html'
     fields = '__all__'
+
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(AddCategoryView, self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
+
+
 class UpdatePostView(UpdateView):
     model = Post
     form_class = EditForm
     template_name = 'update_post.html'
     # fields = ['title', 'body', 'title_tag']
 
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(UpdatePostView, self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
 
 class DeletePostView(DeleteView):
     model = Post
@@ -47,3 +77,8 @@ class DeletePostView(DeleteView):
 def CategoryView(request, catego):
     category_posts = Post.objects.filter(category=catego).order_by('-post_date')
     return render(request, 'categories.html', {'catego':catego.title(), 'category_posts':category_posts})
+
+
+def CategoryListView(request):
+    cat_menu_list = Category.objects.all()
+    return render(request, 'category_list.html', {'cat_menu_list':cat_menu_list})
