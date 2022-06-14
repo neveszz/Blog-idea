6b9b3711abbc197f8ea10d8ac195c98cad320207
neveszz <neveszalves@gmail.com>
2022-06-14 14:38:43 -0300

    added UNlike buttons to the blog pages

--- ablog/theblog/views.py
+++ ablog/theblog/views.py
@@ -28,9 +28,13 @@ class ArticleDetailView(DetailView):
         cat_menu = Category.objects.all()
         context = super(ArticleDetailView, self).get_context_data(*args, **kwargs)
         stuff = get_object_or_404(Post, id=self.kwargs['pk'])
+        liked = False
+        if stuff.likes.filter(id=self.request.user.id).exists():
+            liked = True
         total_likes = stuff.total_likes()
         context["cat_menu"] = cat_menu
         context['total_likes'] = total_likes
+        context['liked'] = liked
         return context
 
 class AddPostView(CreateView):
@@ -88,6 +92,12 @@ def CategoryListView(request):
 
 def LikeView(request, pk):
     post = get_object_or_404(Post, id=request.POST.get('post_id'))
-    post.likes.add(request.user)
+    liked = False
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        liked = False
+    else:
+        post.likes.add(request.user)
+        liked = True
     return HttpResponseRedirect(reverse('article-detail', args=[str(pk)]))
 
