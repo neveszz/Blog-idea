22b28d839dbb318f317beb0fb44881a1982246a9
neveszz <neveszalves@gmail.com>
2022-06-14 20:23:59 -0300

    boostrapped register form

--- ablog/members/views.py
+++ ablog/members/views.py
@@ -3,10 +3,11 @@ from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 # Create your views here.
+from .forms import SignUpForm
 
 
 class UserRegistrationView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = SignUpForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
 
