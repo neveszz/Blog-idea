05f6c7b829a166ec3df526a9c47357246e06dbc2
neveszz <neveszalves@gmail.com>
2022-06-15 12:00:30 -0300

    edit profile page

--- ablog/members/urls.py
+++ ablog/members/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import UserRegistrationView
+from .views import UserRegistrationView, UserEditView
+
 urlpatterns = [
     path('register/',UserRegistrationView.as_view(), name='register'),
+    path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
 
 
 ]
--- ablog/members/views.py
+++ ablog/members/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
 # Create your views here.
 from .forms import SignUpForm
@@ -11,4 +11,11 @@ class UserRegistrationView(generic.CreateView):
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
 
+class UserEditView(generic.UpdateView):
+    form_class = UserChangeForm
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self, queryset=None):
+        return self.request.user
 
