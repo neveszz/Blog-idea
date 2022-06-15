0809437660effb68e1fea67694694dd75a02498f
neveszz <neveszalves@gmail.com>
2022-06-15 19:23:40 -0300

    header image

--- ablog/theblog/models.py
+++ ablog/theblog/models.py
@@ -16,6 +16,7 @@ class Category(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
+    header_image = models.ImageField(null=True, blank=True, upload_to='images/')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     # body = models.TextField()
     body = RichTextField(blank=True, null=True)
